f3bf48f8436b374bb03f755aac07c0b4646f7570
avr567 <avr16@pitt.edu>
2018-11-14 16:36:08 -0500

    added correct flag

--- docker.sh
+++ docker.sh
@@ -2,10 +2,10 @@ docker rm quacc
 docker build -t quacc .
 docker run --name quacc quacc
 ID=$(docker ps -aqf "name=quacc")
-docker cp $ID:/app/report.html .
-docker cp $ID:/app/bitcoin/lcov/ bitcoin/
-docker cp $ID:/app/bitcoincash/lcov/ bitcoincash/
-docker cp $ID:/app/dash/lcov/ dash/
-docker cp $ID:/app/monero/lcov/ monero/
-docker cp $ID:/app/litecoin/lcov/ litecoin/
-docker cp $ID:/app/zcash/lcov/ zcash/
+docker cp -r $ID:/app/report.html .
+docker cp -r $ID:/app/bitcoin/lcov/ bitcoin/
+docker cp -r $ID:/app/bitcoincash/lcov/ bitcoincash/
+docker cp -r $ID:/app/dash/lcov/ dash/
+docker cp -r $ID:/app/monero/lcov/ monero/
+docker cp -r $ID:/app/litecoin/lcov/ litecoin/
+docker cp -r $ID:/app/zcash/lcov/ zcash/
